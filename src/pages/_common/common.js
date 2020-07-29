0c25b126678622dffcaa0b743e3c4af3cc5dcc41
DenBakh <den.bah89@gmail.com>
2020-07-29 07:44:16 +0700

    GulpTemplate v1.0

---
+++ src/pages/_common/common.js
@@ -0,0 +1,33 @@
+//===============Функция перемещения картинки из src HTML в background==================
+import '~/components/blocks/header/header.js'
+
+function ibg() {
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			//console.log($(this).find('img').attr('src'))
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+ibg();
+
+//Проверяем поддержку webp и добавляем класс, чтобы подставлять изображения webp
+/*
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+*/
